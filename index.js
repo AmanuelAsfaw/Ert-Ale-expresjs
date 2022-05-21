f58b66f9d857e25c458f59051ce413e7b2ca9de3
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-21 11:32:34 +0300

    Correct cors error while using api

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
+app.use('/posts', postsRoutes)
+
 const CONNECTION_URL = 'mongodb+srv://amanuel:amanuel123@cluster0.mvclo.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
